d3ab2e9bfa863ae28043b19e6a66e2e78c5c4471
Camille Meulien <cmeulien@heliostech.fr>
2021-09-21 12:01:52 +0300

    Add mermaid plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/openware/open-finance/edit/main/website/',
+          remarkPlugins: [require('mdx-mermaid')],
         },
         blog: {
           showReadingTime: true,
