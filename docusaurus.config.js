f8581e0475d1f352f5aa32f598e4f3570566ed2e
Narupon Srisantitham <5027515+TeeSrisantitham@users.noreply.github.com>
2021-10-12 23:25:39 +0700

    feature: add custody document (#16)
    * feature: add custody document
    
    * Update docusaurus.config.js
    
    Co-authored-by: Tee <tee@yelllow.com>
    Co-authored-by: Louis <lbellet@openware.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/openware/open-finance/edit/main/',
+          editUrl: 'https://github.com/openware/open-finance/edit/master/',
           remarkPlugins: [require('mdx-mermaid')],
         },
         theme: {
